6392f8c151bb8c1400c733788fe611dd720a622a
BeliaevDmitry <dimkabalachonov@gmail.com>
2026-04-05 01:01:25 +0300

    Make subject report type markers configurable via method constants

--- GeneralVsoko/src/main/java/org/school/analysis/service/ComparativeReportService.java
+++ GeneralVsoko/src/main/java/org/school/analysis/service/ComparativeReportService.java
@@ -4,5 +4,6 @@ import java.io.File;
 
 public interface ComparativeReportService {
     File generateEgkrEgeComparativeReport(String school, String currentAcademicYear);
-}
 
+    File generateEgkrEgeSubjectComparativeReport(String school, String currentAcademicYear);
+}
